aceebca358ea0b831274326ecb0fcccbdca71ae5
Aniket Rai <aniketrai@Anikets-MacBook-Air.local>
2026-06-16 16:26:54 +0530

    Added environment variable support for database

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://localhost/helixai_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://localhost/helixai_db"
+)
 
 engine = create_engine(DATABASE_URL)
 
