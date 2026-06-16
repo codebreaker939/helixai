c5ffb038cd56fb49da3679bb348fb62afae34018
Aniket Rai <aniketrai@Anikets-MacBook-Air.local>
2026-06-16 15:50:37 +0530

    Implemented patient CRUD APIs with PostgreSQL

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 from database import SessionLocal
 from models import Patient
 
 app = FastAPI()
+
+
+class PatientCreate(BaseModel):
+    full_name: str
+    age: int
+    gender: str
+    disease: str
+
 
 @app.get("/")
 def home():
@@ -10,6 +20,14 @@ def home():
         "platform": "HelixAI",
         "status": "Running"
     }
+
+
+@app.get("/health")
+def health():
+    return {
+        "service": "healthy"
+    }
+
 
 @app.get("/patients")
 def get_patients():
@@ -32,3 +50,29 @@ def get_patients():
     db.close()
 
     return result
+
+
+@app.post("/patients")
+def create_patient(patient: PatientCreate):
+
+    db = SessionLocal()
+
+    new_patient = Patient(
+        full_name=patient.full_name,
+        age=patient.age,
+        gender=patient.gender,
+        disease=patient.disease
+    )
+
+    db.add(new_patient)
+    db.commit()
+    db.refresh(new_patient)
+
+    patient_id = new_patient.id
+
+    db.close()
+
+    return {
+        "message": "Patient created successfully",
+        "id": patient_id
+    }
